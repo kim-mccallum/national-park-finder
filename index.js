d9f951639fb054b1c0b47ac3a7dc3db4afc31e9c
Kim McCallum <kchaase@gmail.com>
2020-01-01 08:37:43 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// NPS apiKey from https://www.nps.gov/subjects/developer/get-started.htm
+const apiKey = 'dk3vuQY3nthQ9D3xHVBp6bqDdIdjeKx8YvU1Vfoz'
+
+const searchURL = '';
+
+function formatQueryParams(params) {
+    console.log("formatQueryParms ran");
+}
+
+function displayResults(responseJSON){
+    console.log("displayResults ran");
+}
+
+//define params, query, search and fetch!
+function getNationalParks(query, maxResults=10){
+    console.log("getNationalParks ran");
+}
+
+//Get form values and run getNationalParks with values
+function watchForm(){
+    console.log("watchForm ran");
+}
+
+$(watchForm);
